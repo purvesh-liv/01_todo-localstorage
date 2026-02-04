0986ff01978080b6cc1da195875ef51a73b571d2
s4179869 <s4179869@student.rmit.edu.au>
2026-02-05 01:24:14 +1100

    added localstorage function

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const todoInput = document.getElementById("todo-input")
-const addTaskButton =document.getElementById("add-task-btn");
-const TodoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded",()=>{
+    const todoInput = document.getElementById("todo-input");
+    const addTaskButton = document.getElementById("add-task-btn");
+    const TodoList = document.getElementById("todo-list");
 
-let tasks =[]
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+     
+    tasks.forEach(task => renderTask(task))
+        
 
-addTaskButton.addEventListener("click",function(){
-    const tasktext = todoInput.value.trim()
-    if (tasktext === "") return;
 
-    const newTask={
+    addTaskButton.addEventListener("click", function () {
+      const tasktext = todoInput.value.trim();
+      if (tasktext === "") return;
+
+      const newTask = {
         id: Date.now(), // creates a unique id
         Text: tasktext,
-        completed: false
+        completed: false,
+      };
+      tasks.push(newTask); // push to array
+      savedTaks(); // save to localstorage
+      todoInput.value = ""; //clear input
+      console.log(tasks);
+    });
+    function renderTask(task) {}
+
+    function savedTaks() {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
-    tasks.push(newTask) // push to array
-    todoInput.value = "" //clear input
-    console.log(tasks);
-    
+
+
+
+
+
+
+
+
 })
