0a2b61a989264eb906bb31a3cc567b40433d8d46
s4179869 <s4179869@student.rmit.edu.au>
2026-02-05 01:08:10 +1100

    added array and storing

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const todoInput = document.getElementById("todo-input")
+const addTaskButton =document.getElementById("add-task-btn");
+const TodoList = document.getElementById("todo-list");
 
+let tasks =[]
+
+addTaskButton.addEventListener("click",function(){
+    const tasktext = todoInput.value.trim()
+    if (tasktext === "") return;
+
+    const newTask={
+        id: Date.now(), // creates a unique id
+        Text: tasktext,
+        completed: false
+    }
+    tasks.push(newTask) // push to array
+    todoInput.value = "" //clear input
+    console.log(tasks);
+    
+})
